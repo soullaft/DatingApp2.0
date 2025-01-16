a03bbc6cada5dbd1f218c4f36aba4727d65e6246
soullaft <soullaft@gmail.com>
2025-01-16 20:02:34 +0400

    added UsersController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext dataContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync() => Ok(await dataContext.Users.ToListAsync());
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUserAsync(int id)
+        {
+            var user = await dataContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound($"User with id {id} doesn't exist");
+            }
+
+            return Ok(user);
+        }
+    }
+}
